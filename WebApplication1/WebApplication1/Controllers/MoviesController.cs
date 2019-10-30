571dc9ca44eea409a43872fa3063b47882527bb7
Julie YILDIRIM <Julie.YILDIRIM@bpgo.fr>
2019-10-30 16:37:22 +0100

    projet mcv

---
+++ WebApplication1/WebApplication1/Controllers/MoviesController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+using WebApplication1.ViewModels;
+
+namespace WebApplication1.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public IActionResult Random()
+        {
+            var movie = new Movie() {Name = "Shrek!"};
+            var customers = new List<Customer>
+            {
+                new Customer{Name="Customer 1"},
+                new Customer{Name="Customer 2"}
+                };
+
+            var ViewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            
+            return View(ViewModel);
+
+            //return Content("hello world");
+            //return HttpNotFound();
+            //return new EmptyResult();
+        }
+    }
+}
